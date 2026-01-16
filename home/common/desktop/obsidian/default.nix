5c64cf98fa6ed3c49486e4b7741aa3bc4729972e
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-16 16:32:02 -0300

    Update default.nix

--- home/common/desktop/obsidian/default.nix
+++ home/common/desktop/obsidian/default.nix
@@ -12,3 +12,20 @@
     '')
   ];
 }
+
+#{
+# Se você quiser substituir o Obsidian padrão (ao invés de criar outro comando):
+#{
+#home.packages = [
+#  (pkgs.symlinkJoin {
+#    name = "obsidian";
+#    paths = [ pkgs.obsidian ];
+#    buildInputs = [ pkgs.makeWrapper ];
+#    postBuild = ''
+#      wrapProgram $out/bin/obsidian \
+#        --add-flags "--enable-features=WaylandWindowDecorations" \
+#        --add-flags "--ozone-platform=wayland"
+#    '';
+#  })
+#];
+#}
