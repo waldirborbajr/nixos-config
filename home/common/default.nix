23b069bfdfff7e508af0fa9823238656ab125842
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-15 23:14:54 -0300

    Update default.nix

--- home/common/default.nix
+++ home/common/default.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./git/default.nix
+    ./anydesk/default.nix
     ./alacritty/default.nix
     ./btop/default.nix
     ./zsh/default.nix
@@ -10,6 +11,7 @@
     ./rust/default.nix
     ./neovim/default.nix
     ./tmux/default.nix
+    ./k8s/default.nix
   ];
 
   home.stateVersion = "25.11";
