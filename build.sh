2d019a791076a9da3736b291a1bad87bd79eefe8
Waldirborbajr <wborbajr@gmail.com>
2026-01-16 19:52:03 -0300

    wip

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ git pull
 git add .
 git commit -m "wip" 
 git push
-# sudo nixos-rebuild switch --flake .#dell
-make flake-update
-make nixos-rebuild HOST=dell
+sudo nixos-rebuild switch --flake .#dell --impure
+# make flake-update
+# make nixos-rebuild HOST=dell
