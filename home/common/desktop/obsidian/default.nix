faf9d5ff1d5b0bfe5d4fd577e74aaf68c41c19bf
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-16 16:29:27 -0300

    Update default.nix

--- home/common/desktop/obsidian/default.nix
+++ home/common/desktop/obsidian/default.nix
@@ -1,7 +1,14 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    obsidian
+  home.packages = [
+    pkgs.obsidian
+
+    # Wrapper Wayland para Obsidian
+    (pkgs.writeShellScriptBin "obsidian-wayland" ''
+      exec ${pkgs.obsidian}/bin/obsidian \
+        --enable-features=WaylandWindowDecorations \
+        --ozone-platform=wayland "$@"
+    '')
   ];
 }
