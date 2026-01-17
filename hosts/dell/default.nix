4ea3bcc2d1461452f27a517aaa9af42980260980
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-16 23:11:20 -0300

    Update default.nix

--- hosts/dell/default.nix
+++ hosts/dell/default.nix
@@ -1,18 +1,10 @@
 { config, pkgs, ... }:
-
 {
-  imports = [
-    ./hardware-configuration.nix
-  ];
+  imports = [ ./hardware-configuration.nix ];
 
-  ############################################
-  # Host
-  ############################################
   networking.hostName = "dell";
 
-  ############################################
   # Bootloader
-  ############################################
   boot.loader.grub = {
     enable = true;
     device = "/dev/sda";
@@ -20,18 +12,14 @@
     timeout = 2;
   };
 
-  ############################################
-  # Kernel (compatível com Broadcom WL)
-  ############################################
+  # Kernel — 6.6 ainda costuma funcionar melhor com broadcom-sta do que kernels muito novos
   boot.kernelPackages = pkgs.linuxPackages_6_6;
 
-  ############################################
   # Wi-Fi — Broadcom BCM4315 [14e4:4315]
-  ############################################
   nixpkgs.config.allowUnfree = true;
 
-  boot.extraModulePackages = [
-    config.boot.kernelPackages."broadcom-wl"
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    broadcom_sta
   ];
 
   boot.kernelModules = [ "wl" ];
@@ -41,33 +29,23 @@
     "bcma"
     "ssb"
     "brcmsmac"
-    "iwlwifi"
+    "iwlwifi"   # só por garantia mesmo
   ];
 
-  ############################################
   # CPU / Power
-  ############################################
   powerManagement.cpuFreqGovernor = "schedutil";
 
-  ############################################
   # Input
-  ############################################
   services.libinput.enable = true;
 
-  ############################################
-  # Keyboard
-  ############################################
+  # Teclado BR
   services.xserver.xkb.layout = "br";
   console.keyMap = "br-abnt2";
 
-  ############################################
   # RTC
-  ############################################
   time.hardwareClockInLocalTime = false;
 
-  ############################################
   # I/O Scheduler
-  ############################################
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="nvme*", ATTR{queue/scheduler}="none"
     ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/scheduler}="mq-deadline"
