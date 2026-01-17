a4c12d2fdb78fd6561feee0e5badc2666bb14103
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-17 00:11:12 -0300

    Update default.nix

--- home/common/core/comitizen/default.nix
+++ home/common/core/comitizen/default.nix
@@ -1,11 +1,27 @@
 { pkgs, lib, config, ... }:
 
 {
+  ############################################
+  # Commitizen + cz-git
+  ############################################
   home.packages = with pkgs; [
     commitizen
     nodePackages.cz-git
   ];
 
+  ############################################
+  # .czrc (global, declarativo)
+  ############################################
+  home.file.".czrc".text = ''
+    {
+      "path": "cz-git",
+      "useEmoji": true
+    }
+  '';
+
+  ############################################
+  # Aliases
+  ############################################
   programs.zsh.shellAliases = lib.mkIf config.programs.zsh.enable {
     cz  = "cz";
     ccm = "cz commit";
