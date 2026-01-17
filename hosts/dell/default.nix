8b2ca31972f44da07740d932970f7ed0c0e1ca17
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-16 22:56:12 -0300

    Update default.nix

--- hosts/dell/default.nix
+++ hosts/dell/default.nix
@@ -1,119 +1,13 @@
-# # { ... }:
-
-# # {
-# #   imports = [
-# #     ./hardware-configuration.nix
-# #   ];
-
-# #   networking.hostName = "dell";
-
-# #   ############################################
-# #   # Bootloader
-# #   ############################################
-# #   boot.loader.grub = {
-# #     enable = true;
-# #     device = "/dev/sda";
-# #     useOSProber = true;
-# #   };
-
-# #   ############################################
-# #   # Keyboard (Dell = PT-BR)
-# #   ############################################
-# #   services.xserver.xkb = {
-# #     layout = "br";
-# #     variant = "";
-# #   };
-
-# #   console.keyMap = "br-abnt2";
-# # }
-
-# { ... }:
-
-# {
-#   imports = [
-#     ./hardware-configuration.nix
-#   ];
-
-#   ############################################
-#   # Host
-#   ############################################
-#   networking.hostName = "dell";
-
-#   ############################################
-#   # Bootloader
-#   ############################################
-#   boot.loader.grub = {
-#     enable = true;
-#     device = "/dev/sda";
-#     useOSProber = false;
-#     timeout = 2;
-#   };
-
-#   # Dell Inspiron – Intel Wi-Fi
-#   boot.kernelModules = [ "iwlwifi" ];
-
-#   boot.extraModprobeConfig = ''
-#     options iwlwifi power_save=0
-#   '';
-
-
-#   ############################################
-#   # CPU / Power
-#   ############################################
-#   powerManagement.cpuFreqGovernor = "schedutil";
-
-#   ############################################
-#   # Firmware / Microcode
-#   ############################################
-#   hardware.enableRedistributableFirmware = true;
-#   hardware.cpu.intel.updateMicrocode = true;
-
-#   ############################################
-#   # Keyboard (Dell = PT-BR)
-#   ############################################
-#   services.xserver.xkb = {
-#     layout = "br";
-#     variant = "";
-#   };
-
-#   console.keyMap = "br-abnt2";
-
-#   ############################################
-#   # Input (Wayland friendly)
-#   ############################################
-#   services.libinput.enable = true;
-
-#   ############################################
-#   # RTC
-#   ############################################
-#   time.hardwareClockInLocalTime = false;
-
-#   ############################################
-#   # I/O Scheduler (SSD / NVMe)
-#   ############################################
-#   services.udev.extraRules = ''
-#     ACTION=="add|change", KERNEL=="nvme*", ATTR{queue/scheduler}="none"
-#     ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/scheduler}="mq-deadline"
-#   '';
-
-#   ############################################
-#   # Dell Inspiron – Wi-Fi Intel
-#   ############################################
-#   boot.kernelModules = [ "iwlwifi" ];
-
-#   boot.extraModprobeConfig = ''
-#     options iwlwifi power_save=0
-#   '';
-
-# }
-
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [
     ./hardware-configuration.nix
   ];
 
+  ############################################
+  # Host
+  ############################################
   networking.hostName = "dell";
 
   ############################################
@@ -127,11 +21,28 @@
   };
 
   ############################################
-  # Intel Wi-Fi (Dell Inspiron)
+  # Wi-Fi — Broadcom BCM4315 [14e4:4315]
   ############################################
-  boot.extraModprobeConfig = ''
-    options iwlwifi power_save=0
-  '';
+  # Driver proprietário (wl) — mais estável
+  nixpkgs.config.allowUnfree = true;
+
+  hardware.broadcom.wl.enable = true;
+
+  boot.kernelModules = [ "wl" ];
+
+  boot.blacklistedKernelModules = [
+    "b43"
+    "bcma"
+    "ssb"
+    "brcmsmac"
+    "iwlwifi"
+  ];
+
+  ############################################
+  # Kernel (compatível com wl)
+  ############################################
+  # Broadcom wl é mais estável aqui
+  boot.kernelPackages = pkgs.linuxPackages_6_6;
 
   ############################################
   # CPU / Power
@@ -155,10 +66,10 @@
   time.hardwareClockInLocalTime = false;
 
   ############################################
-  # I/O Scheduler (SSD / NVMe)
+  # I/O Scheduler (SSD / HDD)
   ############################################
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="nvme*", ATTR{queue/scheduler}="none"
     ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/scheduler}="mq-deadline"
-  '';  
+  '';
 }
