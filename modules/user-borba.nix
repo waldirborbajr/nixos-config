777328ae270a6b2144b1b384651c1ecc1403d56d
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-19 20:27:04 -0300

    Update user-borba.nix

--- modules/user-borba.nix
+++ modules/user-borba.nix
@@ -7,8 +7,6 @@
   users.users.borba = {
     isNormalUser = true;
     description = "BORBA JR W";
-
-    # Default shell
     shell = pkgs.zsh;
 
     extraGroups = [
@@ -19,38 +17,6 @@
       "dialout"   # required for CHIRP / serial radios
     ];
   };
-
-  ############################################
-  # Zsh shell initialization for user
-  ############################################
-  programs.zsh = {
-    enable = true;
-
-    shellInit = ''
-      # Clipboard alias (Wayland/X11)
-      if command -v wl-copy &>/dev/null && [ -n "$WAYLAND_DISPLAY" ]; then
-        alias clip='wl-copy'
-        alias paste='wl-paste'
-      elif command -v xclip &>/dev/null && [ -n "$DISPLAY" ]; then
-        alias clip='xclip -selection clipboard'
-        alias paste='xclip -selection clipboard -o'
-      fi
-    '';
-  };
-
-  ############################################
-  # Auto-login
-  ############################################
-  services.xserver.displayManager.autoLogin = {
-    enable = true;
-    user = "borba";
-  };
-
-  ############################################
-  # Disable TTY competition
-  ############################################
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
 
   ############################################
   # Passwordless sudo
