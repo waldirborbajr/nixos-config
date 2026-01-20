c08e640f9f2f57f34582b66674eb0d0fe1aefc81
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-20 00:34:25 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,26 +1,29 @@
-# ===========================================================
-# NixOS Configuration - Dell Inspiron 1564
-# ===========================================================
-
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   ############################################
   # Imports
   ############################################
   imports = [
-    ./hardware-configuration-dell.nix       # Gerado pelo nixos-generate-config
+    ./hardware-configuration-dell.nix
+    ./modules/hardware-dell.nix
+    ./modules/hardware-radio-chirp.nix
+    ./modules/kernel-tuning.nix
     ./modules/desktop-gnome.nix
+    ./modules/fonts.nix
     ./modules/system-programs.nix
     ./modules/system-packages.nix
     ./modules/containers/docker.nix
     ./modules/containers/k3s.nix
+    # ./modules/containers/podman.nix   # Desativado enquanto Docker estiver ativo
+    ./modules/maintenance.nix
+    ./modules/maintenance-hm.nix
     ./modules/user-borba.nix
     ./modules/nix-unstable.nix
   ];
 
   ############################################
-  # Hostname
+  # Host
   ############################################
   networking.hostName = "nixos";
 
@@ -45,46 +48,35 @@
   # Nix configuration
   ############################################
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   ############################################
-  # SSH / Firewall
+  # Remote access (SSH)
   ############################################
   services.openssh.enable = true;
   networking.firewall.allowedTCPPorts = [ 22 ];
 
   ############################################
-  # Wi-Fi / Bluetooth - Dell Inspiron 1564
-  ############################################
-  networking.networkmanager.enable = true;
-  hardware.enableRedistributableFirmware = true;
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-
-  boot.initrd.kernelModules = [ "ssb" "b43" "btusb" ];
-  boot.kernelModules = [ "ssb" "b43" ];
-  boot.blacklistedKernelModules = [ "bcma" "brcmsmac" "wl" ];
-
-  environment.systemPackages = with pkgs; [
-    linux-firmware   # Contém firmware Broadcom e outros
-    bluez
-    blueman
-    pciutils
-    usbutils
-    rfkill
-  ];
-
-  ############################################
-  # GNOME Desktop (Wayland)
+  # GNOME + Wayland
   ############################################
   services.xserver.enable = true;
   services.displayManager.gdm.enable = true;
   services.displayManager.gdm.wayland = true;
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "borba";
-  services.gnome.core-apps.enable = true;
+  services.displayManager.gdm.autoSuspend = false;
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = "borba";
+  };
+  services.desktopManager.gnome.enable = true;
+
+  # Força GNOME core-apps para evitar conflito de módulos
+  services.gnome.core-apps.enable = lib.mkForce true;
   services.gnome.gnome-keyring.enable = true;
 
+  # Sessão Wayland
   environment.sessionVariables = {
     XDG_SESSION_TYPE = "wayland";
     QT_QPA_PLATFORM = "wayland";
@@ -93,23 +85,54 @@
     TERMINAL = "alacritty";
   };
 
-  ############################################
-  # Bootloader (GRUB BIOS / Legacy)
-  ############################################
-  boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "/dev/sda" ];  # Disco principal, ajuste se necessário
-  boot.loader.grub.useOSProber = false;
+  # XDG portals
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = with pkgs; [
+    xdg-desktop-portal-gnome
+    xdg-desktop-portal-gtk
+  ];
 
   ############################################
-  # Docker + K3s
+  # Wi-Fi e Bluetooth (Broadcom BCM4312)
+  ############################################
+  networking.networkmanager.enable = true;
+  hardware.enableRedistributableFirmware = true;
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # Firmware Broadcom
+  environment.systemPackages = with pkgs; [
+    linux-firmware
+    bluez
+    blueman
+    b43-fwcutter
+    wirelesstools
+    pciutils
+    usbutils
+    rfkill
+  ];
+
+  # Carregamento dos módulos corretos
+  boot.initrd.kernelModules = [ "ssb" "b43" "btusb" ];
+  boot.kernelModules = [ "ssb" "b43" ];
+  boot.blacklistedKernelModules = [ "bcma" "brcmsmac" "wl" ];
+
+  ############################################
+  # Docker (prioridade)
   ############################################
   virtualisation.docker.enable = true;
   virtualisation.docker.enableOnBoot = true;
-  users.users.borba.extraGroups = [ "docker" ];
 
-  # Podman comentado para uso futuro (quando Docker for desligado)
+  # Podman comentado para uso futuro
   # virtualisation.podman.enable = true;
   # virtualisation.podman.dockerCompat = true;
+
+  ############################################
+  # Bootloader (GRUB)
+  ############################################
+  boot.loader.grub.enable = true;
+  boot.loader.grub.useOSProber = false;
+  boot.loader.grub.devices = [ "/dev/sda" ]; # Dell Inspiron BIOS legacy
 
   ############################################
   # System state version
