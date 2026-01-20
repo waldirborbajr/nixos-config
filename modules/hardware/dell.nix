e4dd09dadc9eaed7ad2929b5f9567d10d979596a
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-20 20:51:51 -0300

    Update dell.nix

--- modules/hardware/dell.nix
+++ modules/hardware/dell.nix
@@ -1,17 +1,10 @@
 { config, pkgs, lib, ... }:
 
 {
-  # Dell ACPI bug: mata Wi-Fi e Bluetooth
-  boot.blacklistedKernelModules = [
-    "dell_laptop"
-    "dell_wmi"
-    "dell_smbios"
-  ];
-
   networking.enableB43Firmware = true;
 
   environment.systemPackages = with pkgs; [
-    b43-fwcutter
+    b43FirmwareCutter
     iw
     rfkill
   ];
