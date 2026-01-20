5ead39c99e8ecfcedb6bbcd755e083f8d1c4cd7d
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-20 02:18:13 -0300

    Update system-packages.nix

--- modules/system-packages.nix
+++ modules/system-packages.nix
@@ -41,7 +41,7 @@ in
     # Editors / IDEs / Git
     ##########################################
     unstable.neovim
-    vscode
+    unstable.vscode
     vscode-extensions.ms-vscode-remote.remote-containers
     lazygit
     git
