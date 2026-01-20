38d9b8691f43562025b4e7cb28dd67b5bbbe697e
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-20 00:24:53 -0300

    Update hardware-dell.nix

--- modules/hardware-dell.nix
+++ modules/hardware-dell.nix
@@ -2,25 +2,27 @@
 
 {
   ############################################
-  # Rede / Wi-Fi e Bluetooth
+  # Wi-Fi e Bluetooth
   ############################################
-  networking.networkmanager.enable = true
-  hardware.enableRedistributableFirmware = true
-  hardware.bluetooth.enable = true
-  services.blueman.enable = true
+  networking.networkmanager.enable = true;
 
-  # Carrega os módulos corretos no initrd
-  boot.initrd.kernelModules = [ "ssb" "b43" "btusb" ]
-  boot.kernelModules = [ "ssb" "b43" ]
+  hardware.enableRedistributableFirmware = true;
+  hardware.bluetooth.enable = true;
+
+  services.blueman.enable = true;
+
+  # Carrega os módulos corretos no initrd e kernel
+  boot.initrd.kernelModules = [ "ssb" "b43" "btusb" ];
+  boot.kernelModules = [ "ssb" "b43" ];
 
   # Evita conflitos com outros drivers Broadcom
-  boot.blacklistedKernelModules = [ "bcma" "brcmsmac" "wl" ]
+  boot.blacklistedKernelModules = [ "bcma" "brcmsmac" "wl" ];
 
   ############################################
   # Pacotes essenciais
   ############################################
   environment.systemPackages = with pkgs; [
-    linux-firmware   # Firmware Broadcom e outros
+    linux-firmware   # contém firmware Broadcom e outros
     bluez            # CLI Bluetooth
     blueman          # GUI Bluetooth
     pciutils
@@ -31,6 +33,6 @@
   ############################################
   # Bootloader GRUB
   ############################################
-  boot.loader.grub.enable = true
-  boot.loader.grub.useOSProber = false
+  boot.loader.grub.enable = true;
+  boot.loader.grub.useOSProber = false; # evita warnings de outros OS
 }
