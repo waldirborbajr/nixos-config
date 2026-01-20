030ad9e91c5a7b873085cfaf9c379f8d63316d14
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-19 23:52:59 -0300

    Update hardware-dell.nix

--- modules/hardware-dell.nix
+++ modules/hardware-dell.nix
@@ -2,72 +2,31 @@
 
 {
   ############################################
-  # Enable NetworkManager (Wi-Fi / Ethernet)
+  # Wi-Fi e Bluetooth Dell
   ############################################
   networking.networkmanager.enable = true;
 
-  ############################################
-  # Enable redistributable firmware (Dell Wi-Fi / BT)
-  ############################################
   hardware.enableRedistributableFirmware = true;
 
-  ############################################
-  # Bluetooth
-  ############################################
   hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
   services.blueman.enable = true;
-
-  ############################################
-  # Audio (PipeWire)
-  ############################################
-  services.pulseaudio.enable = false;
-
-  security.rtkit.enable = true;
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  ############################################
-  # Keyboard — Dell Inspiron (pt_BR)
-  ############################################
-  services.xserver.xkb = {
-    layout = "br";
-    variant = "";
-  };
-  console.keyMap = "br-abnt2";
-
-  ############################################
-  # Bootloader (GRUB)
-  ############################################
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = false;
 
   ############################################
   # Kernel Modules
   ############################################
-  boot.initrd.availableKernelModules = [
-    "ehci_pci"
-    "ahci"
-    "ums_realtek"
-    "usb_storage"
-    "sd_mod"
-    "sr_mod"
-    "b43"       # Broadcom Wi-Fi
-    "btusb"     # Bluetooth USB
+  boot.initrd.kernelModules = [ "b43" "btusb" ];
+  
+  ############################################
+  # Forçar firmware no initrd
+  ############################################
+  boot.initrd.extraFirmware = with pkgs.linuxFirmware; [
+    b43/ucode15.fw
   ];
 
-  boot.kernelModules = [ "kvm-intel" ];
-
   ############################################
-  # System packages for firmware
+  # Pacote de firmware
   ############################################
   environment.systemPackages = with pkgs; [
-    linux-firmware      # Inclui Broadcom b43, btusb e outros firmwares
+    linuxFirmware
   ];
 }
