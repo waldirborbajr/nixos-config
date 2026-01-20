5fa6327d907b27042f2130db99bc0239dae1efd0
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-19 21:09:52 -0300

    Update docker.nix

--- modules/containers/docker.nix
+++ modules/containers/docker.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   ############################################
@@ -6,11 +6,23 @@
   ############################################
   virtualisation.docker = {
     enable = true;
-    enableOnBoot = true;
+
+    # Força Docker para iniciar no boot, sobrescrevendo qualquer outro valor
+    enableOnBoot = lib.mkForce true;
   };
 
   ############################################
   # User access
   ############################################
-  users.users.borba.extraGroups = [ "docker" ];
+  users.users.borba.extraGroups = lib.mkForce (users.users.borba.extraGroups ++ [ "docker" ]);
+
+  ############################################
+  # Podman (rootless / docker-compatible)
+  ############################################
+  # Para usar Podman no futuro (desative Docker primeiro)
+  # virtualisation.podman = {
+  #   enable = true;
+  #   dockerCompat = true;
+  #   defaultNetwork.settings.dns_enabled = true;
+  # };
 }
