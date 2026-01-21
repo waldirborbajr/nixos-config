3d2fcce9072af12c21e8e5ba227b951fa68bf4c1
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-21 20:46:59 -0300

    Update common.nix

--- modules/performance/common.nix
+++ modules/performance/common.nix
@@ -83,9 +83,9 @@
     memoryPercent = 25;
   };
 
-  systemd.extraConfig = ''
-    DefaultTimeoutStartSec=10s
-    DefaultTimeoutStopSec=10s
-  '';
+  systemd.settings.Manager = {
+    DefaultTimeoutStartSec = "10s";
+    DefaultTimeoutStopSec  = "10s";
+  };
 
 }
