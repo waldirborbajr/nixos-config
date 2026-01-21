5239dc2aefab0d9f45c9e5a6be092f6fac27e241
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-21 19:32:48 -0300

    Update enable.nix

--- modules/flatpak/enable.nix
+++ modules/flatpak/enable.nix
@@ -1,26 +1,20 @@
+# modules/flatpak/enable.nix
 { pkgs, ... }:
-
 {
-  ##########################################
-  # Flatpak
-  ##########################################
-
   services.flatpak.enable = true;
 
-  services.flatpak.remotes = [
+  # Add Flathub (system-wide, survives rebuilds)
+  services.flatpak.remotes = lib.mkBefore [  # mkBefore so it can be overridden if needed
     {
       name = "flathub";
-      location = "https://flathub.org/repo/flathub.flatpakrepo";
+      location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
     }
   ];
 
-  ##########################################
-  # XDG Portal (required)
-  ##########################################
-
+  # XDG portal is required for Flatpak apps to access host resources properly
   xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
-  xdg.portal.extraPortals = with pkgs; [
-    xdg-desktop-portal-gtk
-  ];
+  # Optional: if you use GNOME/KDE and want graphical Flatpak updates/installs
+  # environment.systemPackages = [ pkgs.gnome.gnome-software pkgs.gnome.gnome-software ];
 }
