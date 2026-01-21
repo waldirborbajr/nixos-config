b4cb4a8289197ebb36d34f2d37cc42f404acba20
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-21 20:57:18 -0300

    Create ssh.nix

---
+++ modules/ssh.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
+  };
+}
