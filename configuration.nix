ce8edfc1ff1ebd4bf8f083d7401d11c8fa3634dc
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-21 22:45:59 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,13 +1,22 @@
 # configuration.nix
-{ ... }:
+{ config, pkgs, lib, ... }:
 
 {
+  ############################################
+  # Host-specific configurations
+  ############################################
   imports = [
     ./hosts/macbook.nix
     # ./hosts/dell.nix
 
+    ############################################
+    # Core system modules
+    ############################################
     ./core.nix
   ];
 
+  ############################################
+  # NixOS version compatibility
+  ############################################
   system.stateVersion = "25.11";
 }
