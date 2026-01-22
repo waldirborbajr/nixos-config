a88c617eb073ee5cc7ea8a6822fc4278e2dbc80c
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-21 23:15:56 -0300

    Update fonts.nix

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -5,7 +5,7 @@
   ############################################
   fonts = {
     packages = with pkgs; [
-      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+      nerd-fonts.jetbrains-mono
     ];
     fontconfig = {
       enable = true;
