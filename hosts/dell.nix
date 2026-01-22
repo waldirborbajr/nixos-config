8bff7eab40091179c56095a39f4d1c85d90fb7c5
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-21 22:45:11 -0300

    Update dell.nix

--- hosts/dell.nix
+++ hosts/dell.nix
@@ -1,8 +1,33 @@
+{ config, pkgs, lib, ... }:
+
 {
+  ############################################
+  # Hardware
+  ############################################
   imports = [
     ./modules/hardware/dell.nix
     ./modules/performance/dell.nix
-
     ../hardware-configuration-dell.nix
   ];
+
+  ############################################
+  # Host identity
+  ############################################
+  networking.hostName = "dell-nixos";
+
+  ############################################
+  # Bootloader (Legacy BIOS)
+  ############################################
+  boot.loader.grub.enable = true;
+  boot.loader.grub.devices = [ "/dev/sda" ];
+
+  ############################################
+  # Keyboard (Dell)
+  ############################################
+  console.keyMap = "br-abnt2";
+
+  services.xserver.xkb = {
+    layout = "br";
+    variant = "abnt2";
+  };
 }
