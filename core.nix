a298f457ab848be6f4d93bd4b2ff06f4a595b2c3
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-21 21:55:05 -0300

    Create core.nix

---
+++ core.nix
@@ -0,0 +1,47 @@
+{ ... }:
+
+{
+  imports = [
+
+    ##########################################
+    # Core system
+    ##########################################
+    ../modules/base.nix
+    ../modules/networking.nix
+    ../modules/audio.nix
+    ../modules/fonts.nix
+    ../modules/nixpkgs.nix
+
+    ##########################################
+    # Desktop
+    ##########################################
+    ../modules/desktops/gnome.nix
+    ../modules/autologin.nix
+
+    ##########################################
+    # Containers / Virtualization
+    ##########################################
+    ../modules/containers/docker.nix
+    ../modules/containers/k3s.nix
+    ../modules/virtualization/libvirt.nix
+
+    ##########################################
+    # Users / Dev
+    ##########################################
+    ../modules/users/borba.nix
+    ../modules/system-packages.nix
+    ../modules/python/default.nix
+    ../modules/nodejs/default.nix
+
+    ##########################################
+    # Flatpak
+    ##########################################
+    ../modules/flatpak/enable.nix
+    ../modules/flatpak/packages.nix
+
+    ##########################################
+    # Access
+    ##########################################
+    ../modules/ssh.nix
+  ];
+}
