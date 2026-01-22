2dcac45af1b7384f82f3d7fcd2e303a7d437092c
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-21 22:35:53 -0300

    Update dell.nix

--- hosts/dell.nix
+++ hosts/dell.nix
@@ -1,3 +1,8 @@
+{
+  imports = [
+    ./modules/hardware/dell.nix
+    ./modules/performance/dell.nix
 
-../modules/hardware/dell.nix
-../modules/performance/dell.nix
+    ../hardware/hardware-configuration-dell.nix
+  ];
+}
