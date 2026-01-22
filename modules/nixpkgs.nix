23c87fb1d599a09f9377247990f8cf50b5409a7b
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-22 12:44:58 -0300

    Update nixpkgs.nix

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -1,9 +1,9 @@
+{ lib, pkgs, ... }:
+
 let
   unstablePkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-    sha256 = "0hx09ar14njl4vgarsy79vlykwswg7rbxak7c7qm1n8r0szy6r0b";
   }) {
-    system = "x86_64-linux";      # <<<<< ESSENCIAL
     config = { allowUnfree = true; };
   };
 in
@@ -13,33 +13,8 @@ in
 
     overlays = [
       (final: prev: {
-        unstable = import inputs.nixpkgs-unstable {
-          inherit system;
-          config.allowUnfree = true;
-        };
+        unstable = unstablePkgs;
       })
     ];
   };
 }
-
-# # modules/nixpkgs.nix
-# { lib, pkgs, ... }:
-
-# let
-#   unstablePkgs = import (builtins.fetchTarball {
-#     url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
-#   }) {
-#     config = { allowUnfree = true; };
-#   };
-# in
-# {
-#   nixpkgs = {
-#     config.allowUnfree = true;
-
-#     overlays = [
-#       (final: prev: {
-#         unstable = unstablePkgs;
-#       })
-#     ];
-#   };
-# }
