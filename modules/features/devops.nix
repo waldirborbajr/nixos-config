9907fca5619326c2e6d6dbcf8c30343a99043de9
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-23 13:42:23 -0300

    Update devops.nix

--- modules/features/devops.nix
+++ modules/features/devops.nix
@@ -1,30 +1,13 @@
 # modules/features/devops.nix
-# ---
-{ config, lib, pkgs, devopsEnabled ? false, ... }:
+{ lib, devopsEnabled ? false, ... }:
 
-let
-  enable = devopsEnabled;
-in
 {
-  # A ideia: instalar ferramentas pode ser sempre,
-  # mas serviços só sobem quando DEVOPS=1.
-  config = lib.mkMerge [
-    {
-      # Deixa explícito: serviços desativados por padrão
-      virtualisation.docker.enable = lib.mkDefault false;
-      services.k3s.enable = lib.mkDefault false;
-    }
+  virtualisation.docker.enable = lib.mkDefault false;
+  services.k3s.enable = lib.mkDefault false;
 
-    (lib.mkIf enable {
-      # DevOps mode ON
-      virtualisation.docker.enable = true;
-      services.k3s.enable = true;
-
-      # (opcional) garante que sobem no boot quando em modo DevOps
-      virtualisation.docker.enableOnBoot = true;
-
-      # Ajuste comum: docker precisa do group
-      users.users.borba.extraGroups = [ "docker" ];
-    })
-  ];
+  config = lib.mkIf devopsEnabled {
+    virtualisation.docker.enable = true;
+    virtualisation.docker.enableOnBoot = true;
+    services.k3s.enable = true;
+  };
 }
