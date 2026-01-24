91997a5b92550fdb3535d7d4dfb9edf143c6424c
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-24 17:25:05 -0300

    Update borba.nix

--- modules/users/borba.nix
+++ modules/users/borba.nix
@@ -6,7 +6,6 @@
     isNormalUser = true;
     description = "BORBA JR W";
     shell = pkgs.zsh;
-
     extraGroups = [
       "wheel"
       "networkmanager"
@@ -14,20 +13,22 @@
       "libvirtd"
       "dialout"
     ];
-
     ## Relation Truth
-
     #openssh.authorizedKeys.keys = [
-    #  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAI... borba@laptop"
+    # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAI... borba@laptop"
     #];
   };
+
+  # Novo: define o grupo primário (resolve o erro atual)
+  users.groups.borba = {};
+
+  # Opcional: associa explicitamente o grupo ao usuário (boa prática)
+  users.users.borba.group = "borba";
 
   ############################################
   # Security-impact-free kernel tweaks
   ############################################
-
   security.sudo.wheelNeedsPassword = false;
-
   security.sudo.extraRules = [
     {
       users = [ "borba" ];
