2a8e40714962e9f561412a4fedf8603440d90eea
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-02-12 01:56:47 +0000

    fix: update wezterm keybinding from CMD to SUPER for Linux

--- modules/apps/wezterm.nix
+++ modules/apps/wezterm.nix
@@ -23,20 +23,19 @@
         local wezterm = require("wezterm")
         
         return {
-          -- font = wezterm.font("JetBrainsMono Nerd Font"),
-        	font = wezterm.font("JetBrains Mono"),
-        	font_size = 18.0,
-        	enable_tab_bar = false,
-        	window_decorations = "RESIZE",
-        	color_scheme = "Catppuccin Mocha",
-        	window_close_confirmation = "NeverPrompt",
-        	keys = {
-        		{
-        			key = "w",
-        			mods = "CMD",
-        			action = wezterm.action.CloseCurrentTab({ confirm = false }),
-        		},
-        	},
+          font = wezterm.font("JetBrains Mono"),
+          font_size = 18.0,
+          enable_tab_bar = false,
+          window_decorations = "RESIZE",
+          color_scheme = "Catppuccin Mocha",
+          window_close_confirmation = "NeverPrompt",
+          keys = {
+            {
+              key = "w",
+              mods = "SUPER",
+              action = wezterm.action.CloseCurrentTab({ confirm = false }),
+            },
+          },
         }
       '';
     };
