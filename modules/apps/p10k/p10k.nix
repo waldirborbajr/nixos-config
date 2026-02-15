2c5014be14c617dfeab9b9b090b74be08a67cbad
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-02-15 00:11:38 +0000

    modified:   modules/apps/p10k/p10k.nix

--- modules/apps/p10k/p10k.nix
+++ modules/apps/p10k/p10k.nix
@@ -6,6 +6,9 @@
 }:
 {
   config = lib.mkIf config.apps.zsh.enable {
-    home.file.".p10k.zsh".source = ./.p10k.zsh;
+    home.file.".p10k.zsh" = {
+      source = ./.p10k.zsh;
+      force = true;
+    };
   };
 }
