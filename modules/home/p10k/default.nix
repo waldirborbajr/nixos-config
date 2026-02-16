1bd01bd8f6cfafd3d8f2ac15ce947cbf7479824e
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-02-16 14:31:16 +0000

    modified:   modules/home/p10k/default.nix

--- modules/home/p10k/default.nix
+++ modules/home/p10k/default.nix
@@ -6,9 +6,12 @@
 }:
 {
   config = lib.mkIf config.apps.zsh.enable {
-    home.file.".p10k.zsh" = {
-      source = ./.p10k.zsh;
-      force = true;
-    };
+    # Note: powerlevel10k theme is configured in modules/home/zsh/zsh.nix
+    # This module provides optional custom p10k configuration file
+    # Uncomment the line below if you have a custom .p10k.zsh file
+    # home.file.".p10k.zsh" = {
+    #   source = ./.p10k.zsh;
+    #   force = true;
+    # };
   };
 }
