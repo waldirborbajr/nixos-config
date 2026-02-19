e9e6985e9d1a904e1521a56060dcbbdd9acf0287
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-02-19 12:39:11 +0000

    modified:   modules/home/helix/default.nix

--- modules/home/helix/default.nix
+++ modules/home/helix/default.nix
@@ -68,19 +68,3 @@ in
     };
   };
 }
-# modules/apps/helix.nix
-# Helix editor
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
-
-{
-  config = lib.mkIf config.apps.helix.enable {
-    home.packages = with pkgs; [
-      helix
-    ];
-  };
-}
