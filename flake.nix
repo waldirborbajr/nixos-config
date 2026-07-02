275e798aef673cf389b80da0338d71cebbe9a979
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-07-02 12:30:42 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,26 @@
 {
-  description = "borba's NixOS configuration";
+  description = "Master Flake for Borba NixOS Config";
+
   inputs = {
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-unstable,
     ...
   } @ inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = {inherit inputs;};
+      specialArgs = {
+        inherit inputs;
+        pkgs-unstable = import nixpkgs-unstable {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+      };
       modules = [
         ./configuration.nix
       ];
