250e4c6cb9525c6a21c69a53102f1fc935d39d7c
waldirborbajr <wborbajr@gmail.com>
2026-07-01 21:50:54 -0300

    doc: format flake.nix

--- flake.nix
+++ flake.nix
@@ -5,10 +5,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
+      specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
       ];
