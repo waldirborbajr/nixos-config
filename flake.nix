5c1ad06ab9802a43ff75deeb857f8fc4370a5c6f
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-07-01 21:50:06 -0300

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "borba's NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
