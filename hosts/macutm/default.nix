76705afc77f03e011500c54eeadba5a61b7315dc
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-07-04 16:37:11 -0300

    Update default.nix

--- hosts/macutm/default.nix
+++ hosts/macutm/default.nix
@@ -1,5 +1,5 @@
 # hosts/m2utm/default.nix
-{ lib, ... }:
+{ lib, pkgs, pkgs-unstable, ... }:
 
 {
   # Bootloader - EFI (correto para VM no UTM)
@@ -17,6 +17,34 @@
   # Otimizações leves para VM
   boot.kernelParams = [ "mitigations=off" ]; # melhora performance em VMs
 
-  # Opcional: melhor suporte a QEMU/UTM
+  # Melhor suporte a QEMU/UTM
   services.qemuGuest.enable = true;
+
+  # ==================== PACOTES ESPECÍFICOS PARA ESTA MÁQUINA ====================
+  environment.systemPackages = with pkgs; [
+    # === Essenciais para VM ===
+    alacritty
+    rofi
+    feh
+    picom
+    brightnessctl
+    playerctl
+    ffmpeg
+    docker
+
+    # === Desenvolvimento leve ===
+    rust-analyzer
+    rustfmt
+    lua-language-server
+    nixd
+    alejandra
+  ];
+
+  # Programas opcionais (usando mkDefault por segurança)
+  programs.firefox.enable = lib.mkDefault true;
+
+  # Se quiser pacotes do unstable específicos desta máquina:
+  # environment.systemPackages = with pkgs-unstable; [
+  #   neovim
+  # ];
 }
