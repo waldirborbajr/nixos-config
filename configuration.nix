c9ab36826a4a6fa253ac1534c0091846f8ed31a6
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-07-06 13:37:00 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -24,6 +24,7 @@
     "oh-my-posh"
     "helix"
     "git"
+    # add more as needed
   ];
 
   mkDotfileLink = name:
@@ -32,13 +33,13 @@ in {
   _module.args.common = common;
 
   imports = [
-    # sops-nix já vem do flake
+    # hardware-configuration.nix is imported per-host via flake.nix
   ];
 
   # ==================== KERNEL ====================
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # ==================== DOTFILES ====================
+  # ==================== AUTOMATIC DOTFILES ====================
   systemd.tmpfiles.rules =
     (map mkDotfileLink dotfilePrograms)
     ++ [
@@ -46,14 +47,63 @@ in {
       "L+ /home/${username}/.config/zsh - - - - ${dotfilesDir}/zsh/.config/zsh"
     ];
 
-  # ==================== HOST ====================
+  # ==================== SLEEP POLICY ====================
+  systemd.sleep.settings.Sleep = {
+    AllowSuspend = "yes";
+    AllowHibernation = "no";
+    AllowHybridSleep = "no";
+    AllowSuspendThenHibernate = "no";
+    MemorySleepMode = "s2idle";
+  };
+
+  # ==================== NETWORK ====================
   networking.hostName = hostname;
   networking.networkmanager.enable = true;
 
+  networking.firewall.allowedTCPPorts = [22];
+
+  # ==================== BLUETOOTH ====================
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+  # ==================== TIME / LOCALE ====================
   time.timeZone = "America/Sao_Paulo";
 
-  # ==================== LOCALE ====================
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  # ==================== FONTS ====================
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      fira-code
+      nerd-fonts.fira-mono
+      nerd-fonts.fira-code
+      nerd-fonts.droid-sans-mono
+      nerd-fonts.jetbrains-mono
+      libertine
+      noto-fonts-color-emoji
+      nerd-fonts.symbols-only
+    ];
+
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = ["FiraCode Nerd Font" "JetBrainsMono Nerd Font"];
+      };
+    };
+  };
 
   # ==================== USERS ====================
   users.users.${username} = {
@@ -66,14 +116,143 @@ in {
   security.sudo.extraRules = [
     {
       users = [username];
-      commands = [{
-        command = "ALL";
-        options = ["NOPASSWD"];
-      }];
+      commands = [
+        {
+          command = "ALL";
+          options = ["NOPASSWD"];
+        }
+      ];
     }
   ];
 
-  # ==================== SSH (HOST KEY VIA SOPS) ====================
+  # ==================== X11 + i3 + LIGHTDM ====================
+  services.xserver = {
+    enable = true;
+
+    desktopManager.xterm.enable = false;
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        polybar
+        i3status
+        i3blocks
+        rofi
+      ];
+    };
+
+    displayManager.lightdm = {
+      enable = true;
+
+      greeters.gtk = {
+        enable = true;
+
+        theme = {
+          package = pkgs.catppuccin-gtk.override {
+            accents = ["mauve"];
+            size = "standard";
+            variant = "mocha";
+          };
+          name = "catppuccin-mocha-mauve-standard";
+        };
+
+        iconTheme = {
+          package = pkgs.papirus-icon-theme;
+          name = "Papirus-Dark";
+        };
+
+        cursorTheme = {
+          package = pkgs.catppuccin-cursors.mochaDark;
+          name = "catppuccin-mocha-dark-cursors";
+        };
+
+        extraConfig = ''
+          font-name = FiraCode Nerd Font 11
+          xft-antialias = true
+          xft-hintstyle = hintslight
+          indicators = ~host;~spacer;~clock;~spacer;~session;~language;~a11y;~power
+        '';
+      };
+    };
+  };
+
+  services.displayManager.defaultSession = "none+i3";
+
+  services.gnome.gnome-keyring.enable = true;
+  security.polkit.enable = true;
+  programs.i3lock.enable = true;
+
+  # ==================== AUDIO ====================
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # ==================== PROGRAMS ====================
+  nixpkgs.config.allowUnfree = true;
+
+  programs = {
+    firefox.enable = lib.mkDefault true;
+    fish.enable = true;
+  };
+
+  # ==================== PACKAGES ====================
+  environment.systemPackages =
+    (with pkgs; [
+      wget
+      curl
+      git
+      helix
+      tmux
+      bat
+      ripgrep
+      eza
+      btop
+      htop
+      fastfetch
+      oh-my-posh
+      alacritty
+      zsh
+      nixd
+      alejandra
+      rofi
+      feh
+      picom
+      xclip
+      lxappearance
+    ])
+    ++ (with pkgs-unstable; [
+      neovim
+    ]);
+
+  # ==================== NIX ====================
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 3d";
+  };
+
+  nix.optimise.automatic = true;
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # ==================== SSH ====================
+
+  services.openssh = {
+    enable = true;
+
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
+  };
 
   sops = {
     defaultSopsFile = ./secrets/${hostname}.yaml;
@@ -89,47 +268,6 @@ in {
     mode = "0600";
   };
 
-  services.openssh = {
-    enable = true;
-
-    hostKeys = [
-      {
-        path = "/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-    ];
-  };
-
-  networking.firewall.allowedTCPPorts = [22];
-
-  # ==================== X11 + i3 ====================
-  services.xserver.enable = true;
-
-  services.xserver.windowManager.i3.enable = true;
-
-  services.displayManager.defaultSession = "none+i3";
-
-  # ==================== AUDIO ====================
-  security.rtkit.enable = true;
-
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-  };
-
-  # ==================== PACKAGES ====================
-  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages =
-    (with pkgs; [
-      wget curl git helix tmux bat ripgrep eza btop htop fastfetch
-      alacritty zsh nixd alejandra rofi feh picom xclip
-    ])
-    ++ (with pkgs-unstable; [
-      neovim
-    ]);
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
+  # ==================== STATE VERSION ====================
   system.stateVersion = "26.05";
 }
