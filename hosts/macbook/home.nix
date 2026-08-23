dcd21429aae12258fb8320694a5cf48a421ba353
waldirborbaja <wborbajr@gmail.com>
2026-08-23 09:04:35 -0300

    add: packages

--- hosts/macbook/home.nix
+++ hosts/macbook/home.nix
@@ -46,7 +46,7 @@ in {
   # Só afeta o MacBook M2 físico — não impacta mac2011, dell1564, macutm
   # nem macvmf. Adicione aqui o que só faz sentido nesta máquina.
   home.packages = with pkgs; [
-    rapidraw
+   # rapidraw
     darktable
   ];
 }
