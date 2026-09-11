4e7561a1fe27f71b376a97f868c2e817fbd5c294
Waldir Borba Junior <wborbajr@gmail.com>
2026-09-10 21:43:10 -0300

    add: doom emacs config

---
+++ home/configs/doom/config.el
@@ -0,0 +1,163 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+
+(setq doom-font (font-spec :family "Terminess Nerd Font" :size 25 :weight 'medium)
+      doom-variable-pitch-font (font-spec :family "Terminess Nerd Font" :size 25))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'catppuccin)
+(setq catppuccin-flavor 'mocha) ; or 'frappe 'latte, 'macchiato, or 'mocha
+(load-theme 'catppuccin t)
+;; set transparency... I don't think this works so TODO
+(set-frame-parameter (selected-frame) 'alpha '(85 85))
+(add-to-list 'default-frame-alist '(alpha 85 85))
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "/run/media/bashbunni/mememe/more-org/")
+;; you can customize your rss feed at ~/org/elfeed.org. This works because I'm
+;; using +org with my rss plugin. Check out
+;; https://github.com/remyhonig/elfeed-org to see an example.
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; golang formatting set up
+;; use gofumpt
+(after! lsp-mode
+  (setq  lsp-go-use-gofumpt t)
+  )
+;; automatically organize imports
+(add-hook 'go-mode-hook #'lsp-deferred)
+;; Make sure you don't have other goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+;; enable all analyzers; not done by default
+(after! lsp-mode
+  (setq  lsp-go-analyses '((fieldalignment . t)
+                           (nilness . t)
+                           (shadow . t)
+                           (unusedparams . t)
+                           (unusedwrite . t)
+                           (useany . t)
+                           (unusedvariable . t)))
+  )
+
+;; use system clipboard
+;; macos
+;; (require 'pbcopy)
+;; (turn-on-pbcopy)
+
+;; x11
+(use-package! xclip
+  :config
+  (setq xclip-mode t)
+  (setq xclip-select-enable-clipboard t)
+  (setq xclip-method 'xclip))
+
+;; use fish shell by default
+(setq explicit-shell-file-name "/run/current-system/sw/bin/fish")
+
+;; use wayland copy (I found this online, hopefully it works :D)
+;; (when (string-equal (getenv "XDG_SESSION_TYPE") "wayland")
+;;   (executable-find "wl-copy")
+;;   (executable-find "wl-paste")
+;;   (defun my-wl-copy (text)
+;;     "Copy with wl-copy if in terminal, otherwise use the original value of `interprogram-cut-function'."
+;;     (if (display-graphic-p)
+;;         (gui-select-text text)
+;;       (let ((wl-copy-process
+;;              (make-process :name "wl-copy"
+;;                            :buffer nil
+;;                            :command '("wl-copy")
+;;                            :connection-type 'pipe)))
+;;         (process-send-string wl-copy-process text)
+;;         (process-send-eof wl-copy-process))))
+;;   (defun my-wl-paste ()
+;;     "Paste with wl-paste if in terminal. otherwise use the original value of `interprogram-paste-function'"
+;;     (if (display-graphic-p)
+;;         (gui-selection-value)
+;;       (shell-command-to-string "wl-paste --no-newline")))
+;;   (setq interprogram-cut-function #'my-wl-copy)
+;;   (setq interprogram-paste-function #'my-wl-paste))
+
+;; remove LSP delays
+(after! flycheck (setq flycheck-idle-change-delay 0.1))
+(after! lsp-mode
+  (setq lsp-idle-delay 0.1)
+  :custom
+  (setq lsp-completion-enable-additional-text-edit t)
+  (setq lsp-modeline-code-actions-enable t)
+  )   
+
+;; load go-specific dap package
+;; (after! dap-mode
+;;   (require 'dap-dlv-go)
+;;   (dap-ui-mode 1)
+;;   (dap-tooltip-mode 1))   
+
+;; Better debugging
+(use-package! dape)   
