1409dc6e8d756d42e92786321850c67b92384e7d
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-09-15 10:54:47 -0300

    Update functions.zsh

--- home/configs/zsh/functions.zsh
+++ home/configs/zsh/functions.zsh
@@ -278,3 +278,18 @@ nic() {
   tmux select-pane -t "$session_name":1.1
   tmux attach-session -t "$session_name"
 }
+
+# Helix Search
+hxs() {
+	RG_PREFIX="rg -i --files-with-matches"
+	local files
+	files="$(
+		FZF_DEFAULT_COMMAND_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
+			fzf --multi 3 --print0 --sort --preview="[[ ! -z {} ]] && rg --pretty --ignore-case --context 5 {q} {}" \
+				--phony -i -q "$1" \
+				--bind "change:reload:$RG_PREFIX {q}" \
+				--preview-window="70%:wrap" \
+				--bind 'ctrl-a:select-all'
+	)"
+	[[ "$files" ]] && hx --vsplit $(echo $files | tr \\0 " ")
+}
